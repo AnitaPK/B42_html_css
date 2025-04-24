139931f5e714e128d1e58f535f19bb1166152185
AnitaPK <anita.khamitkar@gmail.com>
2025-04-24 16:50:57 +0530

    update piechart

--- fullStackProjects/WorldDashboard/world-dashboard-client/src/components/Continent.jsx
+++ fullStackProjects/WorldDashboard/world-dashboard-client/src/components/Continent.jsx
@@ -1,37 +1,55 @@
-import React, { useEffect, useState } from 'react'
-import {populationByContient} from  '../API/api'
-import {Bar, BarChart, CartesianGrid, Cell, Legend, Pie, PieChart, ResponsiveContainer, Tooltip, XAxis, YAxis} from 'recharts';
+import React, { useEffect, useState } from "react";
+import { populationByContient } from "../API/api";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Cell,
+  Legend,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 const Continent = () => {
-const[continent, setContinent] = useState([])
+  const [continent, setContinent] = useState([]);
 
-const fetchData =async ()=>{
-    const data =await populationByContient();
+  const fetchData = async () => {
+    const data = await populationByContient();
     setContinent(data);
-}
-useEffect(()=>{
+  };
+  useEffect(() => {
     fetchData();
-console.log(continent,"In continent file");
+    console.log(continent, "In continent file");
+  }, []);
 
-},[])
-
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#A28EFF', '#FF66C4', '#999999']
+  const COLORS = [
+    "#0088FE",
+    "#00C49F",
+    "#FFBB28",
+    "#FF8042",
+    "#A28EFF",
+    "#FF66C4",
+    "#999999",
+  ];
 
   // Convert population to number
-  const data = continent.map(item => ({
+  const data = continent.map((item) => ({
     Continent: item.Continent,
-    Population: Number(item.CPopulation)
-  }))
+    Population: Number(item.CPopulation),
+  }));
 
   return (
     <>
       Continent
-<ul>
-    {continent.length > 0 && continent.map((c, i)=>(
-        <li key={i}>{c.Continent}</li>
-    ))}
-    </ul>
-{/* 
+      <ul>
+        {continent.length > 0 &&
+          continent.map((c, i) => <li key={i}>{c.Continent}</li>)}
+      </ul>
+      {/* 
     <div style={{ width: '70%', height: 400 }}>
       <ResponsiveContainer>
         <BarChart data={data} margin={{ top: 20, right: 30, bottom: 20, left: 20 }}>
@@ -43,31 +61,33 @@ const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#A28EFF', '#FF66C4'
         </BarChart>
       </ResponsiveContainer>
     </div> */}
-    <div style={{ width: '100%', height: 400 }}>
-      <ResponsiveContainer>
-        <PieChart>
-          <Pie
-            data={data}
-            dataKey="value"
-            nameKey="name"
-            cx="50%"
-            cy="50%"
-            outerRadius={120}
-            fill="#8884d8"
-            label
-          >
-            {data.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-            ))}
-          </Pie>
-          <Tooltip />
-          <Legend />
-        </PieChart>
-      </ResponsiveContainer>
-    </div>
-
+      <div style={{ width: "100%", height: 400 }}>
+        <ResponsiveContainer>
+          <PieChart>
+            <Pie
+              data={data}
+              dataKey="Population"
+              nameKey="Continent"
+              cx="50%"
+              cy="50%"
+              outerRadius={120}
+              fill="#8884d8"
+              label
+            >
+              {data.map((entry, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={COLORS[index % COLORS.length]}
+                />
+              ))}
+            </Pie>
+            <Tooltip />
+            <Legend />
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Continent
+export default Continent;
